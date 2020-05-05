0499fd8fe190e64c9e405a3c28cdde2d455388b8
Martin Lahuta <martin.lahuta@atos.net>
2020-05-05 12:00:33 +0200

    CSS-167,CSS-168 - unit test for CR and IQ for Fr tmAosVcDemux

---
+++ src/test/java/esa/egos/csts/api/procedures/InformationQueryTmAosVcDemuxTest.java
@@ -0,0 +1,107 @@
+package esa.egos.csts.api.procedures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
+
+import esa.egos.csts.api.functionalresources.FunctionalResourceType;
+import esa.egos.csts.api.functionalresources.values.impl.CstsComplexValue;
+import esa.egos.csts.api.functionalresources.values.impl.CstsIntValue;
+import esa.egos.csts.api.types.Label;
+import esa.egos.csts.sim.impl.frm.Fr;
+
+/**
+ * Test CSTS API information query procedure w/ tmAosVcDemux parameters
+ */
+public class InformationQueryTmAosVcDemuxTest extends InformationQueryFrTestBase
+{
+
+    @Override
+    protected FunctionalResourceType getFunctionalResource()
+    {
+        return Fr.tmAosVcDemux;
+    }
+
+    @BeforeClass
+    public static void setupClass() throws Exception
+    {
+        InformationQueryFrTestBase.setUpClass();
+        
+        testParameters.add(new TestParameter(Fr.TmAosVcDemux.parameter.tmAosVcDemuxResourceStatParamOid, "tmAosVcDemuxResourceStat", 1, 2));
+        testParameters.add(new TestParameter(Fr.TmAosVcDemux.parameter.tmAosVcDemuxVcIdParamOid,
+            CstsComplexValue.of("tmAosVcDemuxVcId",
+                CstsComplexValue.of("tmFrames",
+                    CstsIntValue.of("seqOf", 2),
+                    CstsIntValue.of("seqOf", 3)
+                )
+            ),
+            CstsComplexValue.of("tmAosVcDemuxVcId",
+                CstsComplexValue.of("aosFrames",
+                    CstsIntValue.of("seqOf", 10),
+                    CstsIntValue.of("seqOf", 20),
+                    CstsIntValue.of("seqOf", 30)
+                )
+            )
+        ));
+        testParameters.add(new TestParameter(Fr.TmAosVcDemux.parameter.tmAosVcDemuxClcwExtractionParamOid,
+            CstsComplexValue.of("tmAosVcDemuxClcwExtraction",
+                CstsComplexValue.of("clcwExtractionTmVc",
+                    CstsIntValue.of("tfvn", 1),
+                    CstsIntValue.of("scid", 2),
+                    CstsIntValue.of("vcid", 3)
+                )
+            ),
+            CstsComplexValue.of("tmAosVcDemuxClcwExtraction",
+                CstsComplexValue.of("clcwExtractionAosVc",
+                    CstsIntValue.of("tfvn", 10),
+                    CstsIntValue.of("scid", 20),
+                    CstsIntValue.of("vcid", 30)
+                )
+            )
+        ));
+        testParameters.add(new TestParameter(Fr.TmAosVcDemux.parameter.tmAosVcDemuxGvcidParamOid,
+            CstsComplexValue.of("tmAosVcDemuxGvcid",
+                CstsComplexValue.of("tmGvcid",
+                    CstsComplexValue.of("seqOf",
+                        CstsComplexValue.of("SEQUENCE",
+                            CstsIntValue.of("tfvn", 1),
+                            CstsIntValue.of("scid", 2),
+                            CstsIntValue.of("vcid", 3)
+                        )
+                    ),
+                    CstsComplexValue.of("seqOf",
+                        CstsComplexValue.of("SEQUENCE",
+                            CstsIntValue.of("tfvn", 4),
+                            CstsIntValue.of("scid", 5),
+                            CstsIntValue.of("vcid", 6)
+                        )
+                    )
+                )
+            ),
+            CstsComplexValue.of("tmAosVcDemuxGvcid",
+                CstsComplexValue.of("aosGvcid",
+                    CstsComplexValue.of("seqOf",
+                        CstsComplexValue.of("SEQUENCE",
+                            CstsIntValue.of("tfvn", 10),
+                            CstsIntValue.of("scid", 20),
+                            CstsIntValue.of("vcid", 30)
+                        )
+                    )
+                )
+            )
+        ));
+    }
+    
+    @Override
+    protected List<Label> createDefaultLabelList()
+    {
+        List<Label> ret = new ArrayList<Label>(testParameters.size());
+        for (TestParameter testParameter : testParameters)
+        {
+            ret.add(Label.of(testParameter.oid, getFunctionalResource()));
+        }
+        return ret;
+    }
+    
+}
