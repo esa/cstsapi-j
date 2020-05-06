9fec67a915574ef98e37621f86ce982ec7f17d09
Tereza Dankova <tereza.dankova@atos.net>
2020-05-06 17:12:08 +0200

    CSS-195 junit test for Cyclic Report procedure w/ FR aosMcMux parameters

---
+++ src/test/java/esa/egos/csts/api/procedures/CyclicReportAosMcMuxTest.java
@@ -0,0 +1,62 @@
+package esa.egos.csts.api.procedures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
+
+import esa.egos.csts.api.functionalresources.FunctionalResourceType;
+import esa.egos.csts.api.functionalresources.values.impl.CstsComplexValue;
+import esa.egos.csts.api.functionalresources.values.impl.CstsIntValue;
+import esa.egos.csts.api.types.Label;
+import esa.egos.csts.sim.impl.frm.Fr;
+
+/**
+ * Test CSTS API information query procedure w/ AosMcMux parameters
+ */
+public class CyclicReportAosMcMuxTest extends CyclicReportFrTestBase
+{
+
+    @Override
+    protected FunctionalResourceType getFunctionalResource()
+    {
+        return Fr.aosMcMux;
+    }
+
+    @BeforeClass
+    public static void setupClass() throws Exception
+    {
+        CyclicReportFrTestBase.setUpClass();
+        testParameters.add(new TestParameter(Fr.AosMcMux.parameter.aosMcMuxPresenceOfFrameErrorContrParamOid, "aosMcMuxPresenceOfFrameErrorContr", 10, 30));
+        testParameters.add(new TestParameter(Fr.AosMcMux.parameter.aosMcMuxPresenceOfFrameHeaderErrorContrParamOid, "aosMcMuxPresenceOfFrameHeaderErrorContr", 10, 30));
+        testParameters.add(new TestParameter(Fr.AosMcMux.parameter.aosMcMuxContrParamOid, 
+        		CstsComplexValue.of("aosMcMuxContr",
+                        CstsComplexValue.of("absolutePriority",
+                            CstsIntValue.of("seqOf", 1),
+                            CstsIntValue.of("seqOf", 2),
+                            CstsIntValue.of("seqOf", 3)
+                        )
+                    ),
+                    CstsComplexValue.of("aosMcMuxContr",
+                        CstsComplexValue.of("absolutePriority",
+                            CstsIntValue.of("seqOf", 10),
+                            CstsIntValue.of("seqOf", 22),
+                            CstsIntValue.of("seqOf", 46)
+                        )
+                    )
+                ));
+        testParameters.add(new TestParameter(Fr.AosMcMux.parameter.aosMcMuxResourceStatParamOid, "aosMcMuxResourceStat", 10, 30)); 
+    }
+    
+    @Override
+    protected List<Label> createDefaultLabelList()
+    {
+        List<Label> ret = new ArrayList<Label>(testParameters.size());
+        for (TestParameter testParameter : testParameters)
+        {
+            ret.add(Label.of(testParameter.oid, getFunctionalResource()));
+        }
+        return ret;
+    }
+
+}
