38f955862f34ae79a239948142579fa222f70c9b
Tereza Dankova <tereza.dankova@atos.net>
2020-05-14 14:10:23 +0200

    Small fixes on RocfTsProviderTests

--- src/test/java/esa/egos/csts/api/procedures/CyclicReportRocfTsProviderTest.java
+++ src/test/java/esa/egos/csts/api/procedures/CyclicReportRocfTsProviderTest.java
@@ -9,8 +9,6 @@ import esa.egos.csts.api.functionalresources.FunctionalResourceType;
 import esa.egos.csts.api.functionalresources.values.impl.CstsComplexValue;
 import esa.egos.csts.api.functionalresources.values.impl.CstsIntValue;
 import esa.egos.csts.api.functionalresources.values.impl.CstsNullValue;
-import esa.egos.csts.api.functionalresources.values.impl.CstsOctetStringValue;
-import esa.egos.csts.api.functionalresources.values.impl.CstsStringValue;
 import esa.egos.csts.api.types.Label;
 import esa.egos.csts.sim.impl.frm.Fr;
 
@@ -37,7 +35,6 @@ public class CyclicReportRocfTsProviderTest extends CyclicReportFrTestBase
         testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfLatencyLimitParamOid, "rocfLatencyLimit", 1500, 200));
         testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfRequestedUpdateModeParamOid, "rocfRequestedUpdateMode", 1, 2));
         testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfNumberOfOcfsDeliveredParamOid, "rocfNumberOfOcfsDelivered", 150, 200));
-        testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfSvcInstanceStateParamOid, "rocfSvcInstanceState", 1, 3));
         testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfProdStatParamOid, "rocfProdStat", 1, 2));
 
         testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfPermittedGvcidSetParamOid,
@@ -115,10 +112,6 @@ public class CyclicReportRocfTsProviderTest extends CyclicReportFrTestBase
                 )
             )
         ));
-        testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfResponderPortIdParamOid,
-            CstsStringValue.of("rocfResponderPortId", "respport1"),
-            CstsStringValue.of("rocfResponderPortId", "respport2")
-        ));
         testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfReportingCycleParamOid,
             CstsComplexValue.of("rocfReportingCycle",
                 CstsIntValue.of("reportingOn", 1)
@@ -187,18 +180,6 @@ public class CyclicReportRocfTsProviderTest extends CyclicReportFrTestBase
                 CstsIntValue.of("seqOf", 5),
                 CstsIntValue.of("seqOf", 2)
             )
-        ));
-        testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfSvcInstanceIdParamOid,
-            CstsOctetStringValue.of("rocfSvcInstanceId", new byte[] { 1, 2, 3 }),
-            CstsOctetStringValue.of("rocfSvcInstanceId", new byte[] { 10, 20, 30 })
-        ));
-        testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfInitiatorIdParamOid,
-            CstsStringValue.of("rocfInitiatorId", "init1"),
-            CstsStringValue.of("rocfInitiatorId", "initiator2")
-        ));
-        testParameters.add(new TestParameter(Fr.RocfTsProvider.parameter.rocfResponderIdParamOid,
-            CstsStringValue.of("rocfResponderId", "resp1"),
-            CstsStringValue.of("rocfResponderId", "responder2")
         ));
     }
     
