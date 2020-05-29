be5b0d344dd4d96ff1917f0132d6c13846b6a508
Tereza Dankova <tereza.dankova@atos.net>
2020-05-29 15:09:08 +0200

    CSS-219 junit test for Inform Query procedure w/ FR tdmRcrdBuffer parameters

---
+++ src/test/java/esa/egos/csts/api/procedures/InformationTdmRcrdBufferTest.java
@@ -0,0 +1,87 @@
+package esa.egos.csts.api.procedures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import esa.egos.csts.api.functionalresources.FunctionalResourceType;
+import esa.egos.csts.api.types.Label;
+import esa.egos.csts.sim.impl.frm.Fr;
+
+/**
+ * Test CSTS API information query procedure w/ TdmRcrdBuffer parameters
+ */
+public class InformationTdmRcrdBufferTest extends InformationQueryFrTestBase
+{
+    
+    @Override
+    protected FunctionalResourceType getFunctionalResource()
+    {
+        return Fr.tdmRcrdBuffer;
+    }
+
+    @BeforeClass
+    public static void setupClass() throws Exception
+    {
+        InformationQueryFrTestBase.setUpClass();
+        testParameters.add(new TestParameter(Fr.TdmRcrdBuffer.parameter.tdmRcrdBufferResourceStatParamOid, "tdmRcrdBufferResourceStat", 47, 118));   
+        testParameters.add(new TestParameter(Fr.TdmRcrdBuffer.parameter.tdmRcrdBufferRetentionPolicyParamOid, "tdmRcrdBufferRetentionPolicy", 173, 357));   
+        testParameters.add(new TestParameter(Fr.TdmRcrdBuffer.parameter.tdmRcrdBufferMaxStorageAllocParamOid, "tdmRcrdBufferMaxStorageAlloc", 451, 794));   
+    }
+
+    @Override
+    protected List<Label> createDefaultLabelList()
+    {
+        List<Label> ret = new ArrayList<Label>(testParameters.size());
+        for (TestParameter testParameter : testParameters)
+        {
+            ret.add(Label.of(testParameter.oid, getFunctionalResource()));
+        }
+        return ret;
+    }
+    
+    @Test
+    public void testQueryInformationWithNameSet()
+    {
+        super.testQueryInformationWithNameSet();
+    }
+
+    @Test
+    public void testQueryInformationWithLabelSet()
+    {
+        super.testQueryInformationWithLabelSet();
+    }
+
+    @Test
+    public void testQueryInformationWithFunctionalResourceName()
+    {
+        super.testQueryInformationWithFunctionalResourceName();
+    }
+    
+    @Test
+    public void testQueryInformationWithFunctionalResourceType()
+    {
+        super.testQueryInformationWithFunctionalResourceType();
+    }
+    
+    @Test
+    public void testQueryInformationWithProcedureType()
+    {
+        super.testQueryInformationWithProcedureType();
+    }
+
+    @Test
+    public void testQueryInformationWithProcedureInstanceIdentifier()
+    {
+        super.testQueryInformationWithProcedureInstanceIdentifier();
+    }
+
+    @Test
+    public void testQueryInformationWithEmpty()
+    {
+        super.testQueryInformationWithEmpty();
+    }
+    
+}
