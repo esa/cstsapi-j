929c8ce2e78c613360d3974fd10ef895b6dc74b3
Matteo Renesto <matteo.renesto@telespazio-vega.de>
2021-05-03 11:00:52 +0000

    CSTSAPI-33: Added missing file

---
+++ src/main/java/esa/egos/csts/rtn/cfdp/reduction/CfdpTransferOperation.java
@@ -0,0 +1,9 @@
+package esa.egos.csts.rtn.cfdp.reduction;
+
+import esa.egos.csts.api.enumerations.CstsResult;
+
+public interface CfdpTransferOperation {
+	
+	public CstsResult transferData(byte[] data) ;
+
+}
