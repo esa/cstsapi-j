327f57d00c08c8b6c671fd2505a975efa99e431f
Holger Dreihahn <holger dreihahn@esc01107387.esa-ad.esa.int>
2024-04-02 15:44:37 +0200

    Skip p2 deployment by default

--- deploy-systems-accepted.sh
+++ deploy-systems-accepted.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn deploy -Dproject.repository.url=https://vsdenexus.esoc.esa.int/nexus/content/repositories/systems-accepted -DskipTests
+mvn deploy -Dproject.repository.url=https://vsdenexus.esoc.esa.int/nexus/content/repositories/systems-accepted -DskipTests -Dskip.p2=true
